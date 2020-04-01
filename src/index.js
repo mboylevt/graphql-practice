c47f26d02e95a3e9e05add49335d1494745d9d41
mboylevt <matthew.p.boyle@gmail.com>
2020-04-01 08:59:23 -0400

    Stage

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './styles/index.css'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
+
+import { ApolloProvider } from 'react-apollo'
+import { ApolloClient } from 'apollo-client'
+import { createHttpLink } from 'apollo-link-http'
+import { InMemoryCache } from 'apollo-cache-inmemory' 
+
+const httpLink = createHttpLink({
+	uri: 'http://192.168.1.229:4000'
+	// uri: 'http://localhost:4000'
+})
+
+const client = new ApolloClient({
+	link: httpLink,
+	cache: new InMemoryCache()
+})
 
 ReactDOM.render(
   <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
